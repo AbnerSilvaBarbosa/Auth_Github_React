08dd227fc183f7d80480c0a9c5f4cdeb0c2da72d
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2023-09-09 16:41:18 -0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-undef */
+import express from "express"
+import cors from "cors"
+const app = express()
+
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/getAccessToken", async (req, res) => {
+    const requestToken = req.query.code
+    console.log("token front", requestToken)
+
+    const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + requestToken
+
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        } 
+    }).then(response => { return response.json() })
+        .then(data => {
+            res.json(data)
+        })
+})
+
+app.get("/getUserData", async (req, res) => {
+    req.get("Authorization") // Bearer
+    
+
+    await fetch("https://api.github.com/user", {
+        method: "GET",
+        headers: {
+            "Authorization": req.get("Authorization")
+        }
+    }).then(response => { 
+        
+        return response.json() 
+    })
+    .then((data)=>{
+        res.json(data)
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+import './App.css'
+import { useEffect, useState } from "react"
+
+
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID
+
+function App() {
+
+  const [isLogin, setIsLogin] = useState(false)
+  const [showInfo, setShowInfo] = useState(false)
+  const [userData, setUserData] = useState({})
+
+  function loginWithGithub() {
+    window.location.assign("http://github.com/login/oauth/authorize?client_id=" + CLIENT_ID)
+  }
+
+  async function logoutUser() {
+    localStorage.removeItem("accessToken")
+    setIsLogin(false)
+  }
+
+  async function getAccessToken(codeParam) {
+
+    await fetch("http://localhost:3000/getAccessToken?code=" + codeParam, {
+      method: "GET",
+
+    }).then((response) => {
+      return response.json()
+    }
+    ).then((data) => {
+
+      if (data.access_token) {
+        localStorage.setItem("accessToken", data.access_token)
+        setIsLogin(true)
+      }
+    }
+    ).catch((error) => {
+      console.log(error)
+    }
+    )
+  }
+
+  async function getUserData() {
+    await fetch("http://localhost:3000/getUserData", {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("accessToken")
+      }
+    }).then((response) => {
+       return response.json()
+    }
+    ).then((data) => {
+      console.log(data)
+      setUserData(data)
+      setShowInfo(true)
+      
+    }
+    ).catch((error) => {
+      console.log(error)
+    }
+    )
+  }
+
+  useEffect(() => {
+
+
+    if (localStorage.getItem("accessToken") != null) {
+      setIsLogin(true)
+    }
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get('code')
+
+    if (codeParam && (localStorage.getItem("accessToken") === null)) {
+      getAccessToken(codeParam)
+    }
+  }, [])
+
+  return (
+    <div>
+      {
+        isLogin ? (
+
+          <div>
+            <div className='logout'>
+              <h3>User is logged in </h3>
+              <button onClick={logoutUser}>Logout</button>
+            </div>
+            <div>
+              <div>
+                <button onClick={getUserData}>Get User Data</button>
+              </div>
+            </div>
+            {showInfo ? <div>
+              <h3>User Data</h3>
+              <div>
+                <img className='image' src={userData.avatar_url} alt="avatar" />
+                <p>Username:<strong> {userData.login} </strong></p>
+                <p>Name: {userData.name}</p>
+                <p>Location: {userData.location}</p>
+                <p>Followers: {userData.followers}</p>
+                <p>Following: {userData.following}</p>
+                <p>Public Repos: {userData.public_repos}</p>
+          </div>
+            </div> : null}
+          </div>
+
+
+        ) :
+          <div>
+            <div>
+              <h3>User is not logged in </h3>
+              <button onClick={loginWithGithub}>Login Github</button>
+            </div>
+          </div>
+
+
+      }
+
+
+    </div>
+  )
+}
+
+export default App
