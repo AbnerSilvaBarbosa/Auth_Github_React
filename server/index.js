cad771bb18b5a1f0c6005c093a583bcea724ee84
Abner Silva <abner.barbosa@sou.inteli.edu.br>
2023-09-09 17:01:33 -0300

    FEAT: /health

--- server/index.js
+++ server/index.js
@@ -14,6 +14,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
 app.get("/getAccessToken", async (req, res) => {
     const requestToken = req.query.code
     console.log("token front", requestToken)
